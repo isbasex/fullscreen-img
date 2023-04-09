b554f1fe7c65749d86c4e6093019b4364b7fc225
baiyun <baiyun186@gmail.com>
2023-04-09 23:32:09 +0800

    feat: add test

---
+++ test/index.test.ts
@@ -0,0 +1,7 @@
+import { previewImageInFullscreen } from '../src'
+
+describe('previewImageInFullscreen', function () {
+  it('should Defined', function () {
+    expect(previewImageInFullscreen).toBeDefined()
+  })
+})
